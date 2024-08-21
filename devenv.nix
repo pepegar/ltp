921236172c27820a1353ce74f06b6ed6ec0884fd
Pepe García <pepe@pepegar.com>
2024-08-21 17:19:14 +0200

    devenv

--- devenv.nix
+++ devenv.nix
@@ -1,39 +1,18 @@
-{ pkgs, lib, config, inputs, ... }:
-
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  pkgs,
+  lib,
+  ...
+}: {
+  packages =
+    [pkgs.git]
+    ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
+      frameworks.Security
+    ]);
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
+  languages.rust.enable = true;
+  languages.nix.enable = true;
+  pre-commit.hooks.rustfmt.enable = true;
+  pre-commit.hooks.clippy.enable = true;
+  pre-commit.hooks.alejandra.enable = true;
+  pre-commit.hooks.deadnix.enable = true;
 }
